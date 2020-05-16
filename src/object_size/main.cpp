c0f6be9920956b7f92de11d62d66b8139d8d0fc7
Aakash Bhowmick <aakashbhowmick@gmail.com>
2020-05-16 16:24:16 +0530

    Added object_size test

--- src/object_size/main.cpp
+++ src/object_size/main.cpp
@@ -1,10 +1,33 @@
 #include <benchmark/benchmark.h>
-#include <string>
+#include <vector>
 
-void string_creation(benchmark::State& state)
+/* Creates a class of given size in bytes */
+template <size_t BYTES>
+class SizedObject
 {
+public:
+    void SetByte(size_t byte_index, char val)
+    {
+        bytes[byte_index] = val;
+    }
+
+private:
+    char bytes[BYTES];
+};
+
+
+void object_access(benchmark::State& state)
+{
+    constexpr size_t vector_size = 1000000;
+    std::vector<SizedObject<8>> object_vect(vector_size);
     for(auto a: state)
-        std::string str("Hello World");
+    {
+        for(SizedObject<8>& obj : object_vect)
+        {
+            obj.SetByte(0, 'a');
+            benchmark::ClobberMemory();
+        }
+    }
 }
 
-BENCHMARK(string_creation);
+BENCHMARK(object_access);
