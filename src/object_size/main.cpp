080095fa4576447d72cda894b563cb23eaf0b5b6
Aakash Bhowmick <aakashbhowmick@gmail.com>
2020-05-16 17:11:31 +0530

    Completing object size benchmark

--- src/object_size/main.cpp
+++ src/object_size/main.cpp
@@ -16,13 +16,16 @@ private:
 };
 
 
+template <size_t OBJ_SIZE>
 void object_access(benchmark::State& state)
 {
-    constexpr size_t vector_size = 1000000;
-    std::vector<SizedObject<8>> object_vect(vector_size);
+    constexpr size_t vector_size = 10000;
+    using SizedObjectN = SizedObject<OBJ_SIZE>;
+
+    std::vector<SizedObjectN> object_vect(vector_size);
     for(auto a: state)
     {
-        for(SizedObject<8>& obj : object_vect)
+        for(SizedObjectN& obj : object_vect)
         {
             obj.SetByte(0, 'a');
             benchmark::ClobberMemory();
@@ -30,4 +33,14 @@ void object_access(benchmark::State& state)
     }
 }
 
-BENCHMARK(object_access);
+BENCHMARK_TEMPLATE(object_access, 1<<3)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(object_access, 1<<4)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(object_access, 1<<5)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(object_access, 1<<6)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(object_access, 1<<7)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(object_access, 1<<8)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(object_access, 1<<9)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(object_access, 1<<10)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(object_access, 1<<11)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(object_access, 1<<12)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(object_access, 1<<13)->Unit(benchmark::kMillisecond);
