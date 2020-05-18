ac2405ea4893acf36ad8d69a6806627acfba9d6d
Aakash Bhowmick <aakashbhowmick@gmail.com>
2020-05-18 21:16:15 +0530

    Added 3 scenarios for vector erasure

--- src/remove_if/main.cpp
+++ src/remove_if/main.cpp
@@ -3,6 +3,7 @@
 #include <set>
 #include <algorithm>
 #include <numeric>
+#include <iostream>
 
 std::set<size_t> get_fibonacci_till(size_t max)
 {
@@ -11,11 +12,23 @@ std::set<size_t> get_fibonacci_till(size_t max)
     fibs.push_back(1);
     while(fibs.back() < max)
     {
-        fibs.push_back( *(fibs.end()) + *(fibs.end()-2));
+        fibs.push_back( *(fibs.end()-1) + *(fibs.end()-2));
     }
 
     std::set<size_t> fib_set(fibs.begin(), fibs.end());
     return fib_set;
+}
+
+bool is_odd(size_t i)
+{
+    return i%2 == 1;
+};
+
+void print_vector(const std::vector<size_t>& v)
+{
+    for(size_t v_i : v)
+        std::cout << v_i << " ";
+    std::cout << std::endl;
 }
 
 /* Use std::vector::erase() */
@@ -24,21 +37,26 @@ void vector_erase(benchmark::State& state)
     const size_t max = state.range(0);
 
     std::set<size_t> fib_set = get_fibonacci_till(max);
-    std::vector<size_t> int_array(max, 0);
-    benchmark::DoNotOptimize(int_array);
-    std::iota(int_array.begin(), int_array.end(), 1);
 
     for(auto a: state)
     {
+        state.PauseTiming();  // PAUSE TIMING
+        std::vector<size_t> int_array(max, 0);
+        benchmark::DoNotOptimize(int_array);
+        std::iota(int_array.begin(), int_array.end(), 1);
+        state.ResumeTiming(); // RESUME TIMING
+
         auto itr = int_array.begin();
         while(itr != int_array.end())
         {
-            if( fib_set.find(*itr) != fib_set.end())
+            //if( fib_set.find(*itr) != fib_set.end())
+            if(is_odd(*itr))
                 itr = int_array.erase(itr);
             else
                 ++itr;
         }
         benchmark::ClobberMemory();
+        //print_vector(int_array);
     }
 }
 
@@ -49,22 +67,56 @@ void vector_remove_if(benchmark::State& state)
     const size_t max = state.range(0);
 
     std::set<size_t> fib_set = get_fibonacci_till(max);
-    std::vector<size_t> int_array(max, 0);
-    benchmark::DoNotOptimize(int_array);
-    std::iota(int_array.begin(), int_array.end(), 1);
 
     for(auto a: state)
     {
+        state.PauseTiming();  // PAUSE TIMING
+        std::vector<size_t> int_array(max, 0);
+        benchmark::DoNotOptimize(int_array);
+        std::iota(int_array.begin(), int_array.end(), 1);
+        state.ResumeTiming(); // RESUME TIMING
+
         auto end_itr = std::remove_if(int_array.begin(), int_array.end(), 
              [&](size_t i) -> bool {
-                return fib_set.find(i) != fib_set.end();
+                //return fib_set.find(i) != fib_set.end();
+                return is_odd(i);
              });
         int_array.resize(end_itr - int_array.begin());
 
         benchmark::ClobberMemory();
+        //print_vector(int_array);
+    }
+}
+
+/* Copy to new vector */
+void vector_copy_swap(benchmark::State& state)
+{
+    const size_t max = state.range(0);
+
+    std::set<size_t> fib_set = get_fibonacci_till(max);
+
+    for(auto a: state)
+    {
+        state.PauseTiming();  // PAUSE TIMING
+        std::vector<size_t> int_array(max, 0);
+        benchmark::DoNotOptimize(int_array);
+        std::iota(int_array.begin(), int_array.end(), 1);
+        state.ResumeTiming(); // RESUME TIMING
+
+        std::vector<size_t> temp;
+        for(size_t i : int_array)
+        {
+            if(!is_odd(i))
+                temp.push_back(i);
+        }
+        int_array.swap(temp);
+
+        benchmark::ClobberMemory();
+        //print_vector(int_array);
     }
 }
 
 
-BENCHMARK(vector_erase)->Unit(benchmark::kMicrosecond)->Arg(1000000);
-BENCHMARK(vector_remove_if)->Unit(benchmark::kMicrosecond)->Arg(1000000);
+BENCHMARK(vector_erase)->Unit(benchmark::kMicrosecond)->Arg(10000);
+BENCHMARK(vector_remove_if)->Unit(benchmark::kMicrosecond)->Arg(10000);
+BENCHMARK(vector_copy_swap)->Unit(benchmark::kMicrosecond)->Arg(10000);
